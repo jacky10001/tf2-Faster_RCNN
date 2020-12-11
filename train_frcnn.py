b225c930fffa8cceb2f5f59ff7bd41f80d3993bc
jacky10001 <50270079+jacky10001@users.noreply.github.com>
2020-12-11 23:41:53 +0800

    Test frcnn function by Shapes dataset
    In shapes.py, I modify shapes.py from  my repo of Mask_RCNN-tf2.
    Here, I not use mask prediction. So I remove related function.
    Only use bouding box and class of  object to train.

---
+++ train_frcnn.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Jacky Gao
+@date: Wed Dec  9 00:57:35 2020
+"""
+
+import os
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from frcnn import utils
+from frcnn import model as modellib
+from frcnn import visualize
+from frcnn.model import log
+
+from shapes import ShapesConfig
+from shapes import ShapesDataset
+
+MODEL_DIR = 'log_frcnn'
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+# Local path to trained weights file
+COCO_MODEL_PATH = 'mask_rcnn_coco.h5'
+
+
+#%%
+config = ShapesConfig()
+config.display()
+
+
+#%%
+def get_ax(rows=1, cols=1, size=6):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Change the default size attribute to control the size
+    of rendered images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    return ax
+
+
+#%% Dataset
+# Training dataset
+dataset_train = ShapesDataset()
+dataset_train.load_shapes(1000, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+dataset_train.prepare()
+
+# Validation dataset
+dataset_val = ShapesDataset()
+dataset_val.load_shapes(100, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+dataset_val.prepare()
+
+
+#%% Load and display random samples
+image_ids = np.random.choice(dataset_train.image_ids, 1)
+for image_id in image_ids:
+    image = dataset_train.load_image(image_id)
+    bbox, class_ids = dataset_train.load_bbox(image_id)
+    visualize.display_image(image)
+    visualize.display_instances(image, bbox, class_ids, dataset_train.class_names, ax=get_ax())
+
+
+#%% Create Model
+# Create model in training mode
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+tf.keras.utils.plot_model(model.keras_model, to_file='archi_training.png', show_shapes=True)
+
+
+#%% Which weights to start with?
+init_with = "coco"  # imagenet, coco, or last
+
+if init_with == "imagenet":
+    model.load_weights(model.get_imagenet_weights(), by_name=True)
+elif init_with == "coco":
+    # Load weights trained on MS COCO, but skip layers that
+    # are different due to the different number of classes
+    # See README for instructions to download the COCO weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last(), by_name=True)
+
+
+#%% Train in two stages:
+# 1. Only the heads. Here we're freezing all the backbone layers
+#    and training only the randomly initialized layers
+#    (i.e. the ones that we didn't use pre-trained weights from MS COCO).
+#    To train only the head layers, pass `layers='heads'` to the `train()` function.
+# 2. Fine-tune all layers. For this simple example it's not necessary,
+#    but we're including it to show the process. 
+#    Simply pass `layers="all` to train all layers.
+# 
+# Train the head branches
+# Passing layers="heads" freezes all layers except the head
+# layers. You can also pass a regular expression to select
+# which layers to train by name pattern.
+
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE, 
+            epochs=2, 
+            layers='heads')
+
+
+#%% Fine tune all layers
+# Passing layers="all" trains all layers. You can also 
+# pass a regular expression to select which layers to
+# train by name pattern.
+
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE / 10,
+            epochs=5, 
+            layers="all")
+
+
+#%% Save weights
+# Typically not needed because callbacks save after every epoch
+# Uncomment to save manually
+
+model_path = os.path.join(MODEL_DIR, "faster_rcnn_shapes.h5")
+model.keras_model.save_weights(model_path)
+
+
+#%% Detection
+class InferenceConfig(ShapesConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+inference_config = InferenceConfig()
+
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference", 
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+tf.keras.utils.plot_model(model.keras_model, to_file='archi_inference.png', show_shapes=True)
+
+# Get path to saved weights
+# Either set a specific path or find last trained weights
+# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+model_path = model.find_last()
+
+# Load trained weights
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+
+#%% Test on a random image
+image_id = random.choice(dataset_val.image_ids)
+original_image, image_meta, gt_class_id, gt_bbox =\
+    modellib.load_image_gt(dataset_val, inference_config, image_id)
+
+log("original_image", original_image)
+log("image_meta", image_meta)
+log("gt_class_id", gt_class_id)
+log("gt_bbox", gt_bbox)
+
+visualize.display_instances(original_image, gt_bbox, gt_class_id, 
+                            dataset_train.class_names, figsize=(8, 8))
+
+
+#%%
+results = model.detect([original_image], verbose=1)
+
+r = results[0]
+visualize.display_instances(original_image, r['rois'], r['class_ids'], 
+                            dataset_val.class_names, r['scores'], ax=get_ax())
+
+
+#%% Evaluation
+# Compute VOC-Style mAP @ IoU=0.5
+# Running on 10 images. Increase for better accuracy.
+
+image_ids = np.random.choice(dataset_val.image_ids, 10)
+APs = []
+for image_id in image_ids:
+    # Load image and ground truth data
+    image, image_meta, gt_class_id, gt_bbox =\
+        modellib.load_image_gt(dataset_val, inference_config, image_id)
+    molded_images = np.expand_dims(modellib.mold_image(image, inference_config), 0)
+    # Run object detection
+    results = model.detect([image], verbose=0)
+    r = results[0]
+    # Compute AP
+    AP, precisions, recalls, overlaps =\
+        utils.compute_ap(gt_bbox, gt_class_id, r["rois"], r["class_ids"], r["scores"])
+    APs.append(AP)
+    
+print("mAP: ", np.mean(APs))
+
